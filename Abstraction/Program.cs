05b8bfaff63a6a5de10879da814333ffb1dbfcfb
Botan Elveren <botanelveren@gmail.com>
2025-01-13 16:02:08 +0300

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,50 @@
+﻿public abstract class Calisan
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public string Departman { get; set; }
+
+    public abstract void Gorev(); 
+}
+
+public class YazilimGelistirici : Calisan
+{
+    public override void Gorev()
+    {
+        Console.WriteLine("Yazılım geliştiriyorum.");
+    }
+}
+
+public class ProjeYoneticisi : Calisan
+{
+    public override void Gorev()
+    {
+        Console.WriteLine("Proje yönetiyorum.");
+    }
+}
+
+public class SatisTemsilcisi : Calisan
+{
+    public override void Gorev()
+    {
+        Console.WriteLine("Satış yapıyorum.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        YazilimGelistirici yazılımcı = new YazilimGelistirici();
+        yazılımcı.Ad = "Ahmet";
+        yazılımcı.Soyad = "Can";
+        yazılımcı.Departman = "Bilgi İşlem";
+        yazılımcı.Gorev();
+
+        ProjeYoneticisi projeYoneticisi = new ProjeYoneticisi();
+        projeYoneticisi.Ad = "Ayşe";
+        projeYoneticisi.Soyad = "Yılmaz";
+        projeYoneticisi.Departman = "Proje Yönetimi";
+        projeYoneticisi.Gorev();
+    }
+}
